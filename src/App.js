baf8814f1d6e0b81314b56315cce1a841f81901a
pyl-10 <1654453533@qq.com>
2023-03-18 14:25:20 +0800

    card组件

--- src/App.js
+++ src/App.js
@@ -1,6 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
 
+const todoList = [
+  { title: "开发任务-1", status: "23-03-17 17:18" },
+  { title: "开发任务-3", status: "23-03-17 17:18" },
+  { title: "开发任务-4", status: "23-03-17 17:18" },
+  { title: "测试任务-3", status: "23-03-17 17:18" },
+]
+
+const ongoingList = [
+  { title: "开发任务-2", status: "23-03-17 17:18" },
+  { title: "开发任务-6", status: "23-03-17 17:18" },
+  { title: "测试任务-2", status: "23-03-17 17:18" },
+]
+
+const doneList = [
+  { title: "开发任务-7", status: "23-03-17 17:18" },
+  { title: "开发任务-8", status: "23-03-17 17:18" },
+  { title: "开发任务-9", status: "23-03-17 17:18" },
+  { title: "开发任务-5", status: "23-03-17 17:18" },
+  { title: "测试任务-1", status: "23-03-17 17:18" },
+]
+
+//  看板-卡片 组件
+const KanbanCard = ({ title, status }) => {
+  return (
+    <li className="kanban-card">
+      <div className="card-title">{title}</div>
+      <div className="card-status">{status}</div>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div className="App">
@@ -12,23 +43,21 @@ function App() {
         <section className="kanban-column column-todo">
           <h2>待处理</h2>
           <ul>
-            {
-              new Array(10).fill('').map(item => (
-                <li className="kanban-card">
-                  <div className="card-title">开发任务-1</div>
-                  <div className="card-status">22-05-22 18:15</div>
-                </li>
-              ))
-            }
+            {/* 遍历数组中的对象，作为参数传给卡片组件 */}
+            {todoList.map(props => <KanbanCard {...props} />)}
           </ul>
         </section>
         <section className="kanban-column column-ongoing">
           <h2>进行中</h2>
-          <ul></ul>
+          <ul>
+            {ongoingList.map(props => <KanbanCard {...props} />)}
+          </ul>
         </section>
         <section className="kanban-column column-done">
           <h2>已完成</h2>
-          <ul></ul>
+          <ul>
+            {doneList.map(props => <KanbanCard {...props} />)}
+          </ul>
         </section>
       </main>
     </div>
